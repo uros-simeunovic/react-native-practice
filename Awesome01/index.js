dc8db3bfb173fa14c0ddb4b9af19569aae94fab0
Uros <dixypixy007@gmail.com>
2023-09-04 22:03:42 +0200

    react native start

---
+++ Awesome01/index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './AppPro';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
